17606242259144196b2a8f80f244cf2d06fd915a
Sérgio Júnior <sergiaraujo15@gmail.com>
2024-02-08 17:49:56 -0300

    Aula 1

---
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+namespace RocketseatAuction.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{ 
+    [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetCurrentAuction()
+    {
+        var useCase = new GetCurrentAuctionUseCase();
+        var currentAuction = useCase.Execute();
+
+        if (currentAuction is null)
+            return NoContent();
+
+        return Ok(currentAuction);
+    }
+}
